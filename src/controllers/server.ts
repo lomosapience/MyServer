fac9f8e05252c81042927484ccb24c395c48a8fb
lomosapience <lomosapience@gmail.com>
2017-07-25 00:33:40 +0300

    Google-speech added

--- src/controllers/server.ts
+++ src/controllers/server.ts
@@ -1,5 +1,6 @@
 import {createServer, CORS, queryParser, bodyParser} from "restify";
 import {Students} from "../actions/users-actions";
+import {GoogleSpeech} from "../speech/google-speech";
 
 const DEFAULT_PORT = 4000;
 
@@ -18,7 +19,6 @@ export class Server {
             res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
             res.header('Access-Control-Allow-Headers', 'Content-Type');
             res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-
             res.send(200);
             next();
         });
@@ -35,6 +35,9 @@ export class Server {
 
         let students: Students = new Students();
         this.server.get('/users', students.getUsers.bind(students));
+
+        let googleSpeech: GoogleSpeech = new GoogleSpeech();
+        this.server.get('/talk', googleSpeech.recognizeSpeech.bind(googleSpeech));
 
         this.server.listen(DEFAULT_PORT);
         console.log('Server started on port ' + DEFAULT_PORT);
